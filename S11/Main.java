f65cdf5aed71ff05fb9f37098b9af2db044fdd00
Anton HOME <anton.sherri@gmail.com>
2023-07-26 17:23:17 +0200

    test pull from laptop

--- S11/Main.java
+++ S11/Main.java
@@ -2,6 +2,6 @@ package S11;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pull from home");
+        System.out.println("Pull from laptop");
     }
 }
