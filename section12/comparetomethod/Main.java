1cf7d842944eefa7433eb23ad853daea521a0d1e
Anton WORK <anton.sherri@gmail.com>
2023-07-28 15:22:55 +0200

    Section 12 completed

---
+++ section12/comparetomethod/Main.java
@@ -0,0 +1,10 @@
+package section12.comparetomethod;
+
+public class Main {
+    public static void main(String[] args) {
+        String x = "c";
+        String y = "b";
+
+        System.out.println(x.compareTo(y));
+    }
+}
