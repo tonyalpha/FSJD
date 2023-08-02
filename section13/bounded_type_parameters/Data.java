1c77ca71e82bae41b62e3ba912d48f17c933f187
Anton WORK <anton.sherri@gmail.com>
2023-08-02 13:03:01 +0200

    Section 13 finished

---
+++ section13/bounded_type_parameters/Data.java
@@ -0,0 +1,31 @@
+package section13.bounded_type_parameters;
+
+public class Data<K extends Integer,V extends Main> {
+    private K key;
+    private V value;
+
+    public Data(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+
+    public <E extends Character,N extends Number> void display(E element, N number) {
+        System.out.println("Element: " + element + "\nNumber: " + number);
+    }
+}
---
+++ section13/wildcards_in_generics/Car.java
@@ -0,0 +1,25 @@
+package section13.wildcards_in_generics;
+
+public class Car extends Vehicle {
+    private String carModel;
+
+    public Car(int vehicleId, String carModel) {
+        super(vehicleId);
+        this.carModel = carModel;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carModel='" + carModel + '\'' +
+                '}';
+    }
+    @Override
+    void info() {
+        System.out.println("Car Model is " +getCarModel());
+    }
+}
