d0a3c04990314e4cdbcc96c098078621a85e25aa
Anton Sherri <anton.sherri@facilicom.be>
2023-09-01 12:29:57 +0200

    Section 18 finished

---
+++ section18/_208_debugging/Main.java
@@ -0,0 +1,9 @@
+package section18._208_debugging;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5, y = 0, z;
+        z=x/y;
+        System.out.println(z);
+    }
+}
